0edd1866d628b53bfded1598751fa9e66a94bba5
JiaDingYi <wzy2010416033@163.com>
2021-05-25 16:33:21 +0800

    add interface

--- Assets/AASDK/Platforms/iOS/Bridge/AAInterface.m
+++ Assets/AASDK/Platforms/iOS/Bridge/AAInterface.m
@@ -1,123 +1,58 @@
 #import "AATypes.h"
 #import "AAObjectCache.h"
-#import "AANotificationBridge.h"
+#import "AABridge.h"
 
 static NSString *AAStringFromUTF8String(const char *bytes) { return bytes ? @(bytes) : nil; }
-#pragma mark - AANotification method
 
-AATypeNotificationRef AACreateNotification(AATypeNotificationClientRef *notificationClient) {
-    AANotificationBridge *notification = [[AANotificationBridge alloc] initWithNotificationClientReference:notificationClient];
+#pragma mark - method
+AATypeRef AACreateManager(AATypeClientRef *client) {
+    AABridge *manager = [[AABridge alloc] initSDKWithClientReference:client];
     AAObjectCache *cache = [AAObjectCache sharedInstance];
-    [cache.references setObject:notification forKey:[notification aa_referenceKey]];
-    return (__bridge AATypeNotificationRef)notification;
+    [cache.references setObject:manager forKey:[manager aa_referenceKey]];
+    return (__bridge AATypeRef)manager;
 }
 
-void AASetNotificationCallbacks(
-        AATypeNotificationClientRef notification,
-        // 隐私弹框已经展示
-        AAPrivacyPolicyViewControllerHasBeenShownCallback privacyPolicyViewControllerHasBeenShownCallback,
-        // 用户同意隐私政策
-        AAUserAgreesToPrivacyPolicyCallback userAgreesToPrivacyPolicyCallback,
-        // 开始展示用户登录界面
-        AALoginViewControllerHasBeenShownCallback loginViewControllerHasBeenShownCallback,
-        // 登录界面消失
-        AALoginViewControllerHasBeenDismissedCallback loginViewControllerHasBeenDismissedCallback,
+void AASetManagerCallbacks(
+        AATypeClientRef manager,
         // 登录成功
         AALoginSuccessCallback loginSuccessCallback,
         // 登录失败
         AALoginFailCallback loginFailCallback,
-        // 实名认证界面已经展示
-        AAUserAuthVcHasBeenShownCallback userAuthVcHasBeenShownCallback,
         // 实名认证成功
         AAUserAuthSuccessCallback userAuthSuccessCallback,
-        // warning vc已展示
-        AAWarningVcHasBeenShownCallback warningVcHasBeenShownCallback,
-        // 用户支付失败时，在提示界面点击登录
-        AAUserClickLoginButtonInPaymentWarningVcCallback userClickLoginButtonInPaymentWarningVcCallback,
-        // 用户游戏时长不足时，在提示界面点击登录
-        AAUserClickLoginButtonInNoTimeLeftWarningVcCallback userClickLoginButtonInNoTimeLeftWarningVcCallback,
-        // 用户在提示界面点击退出游戏
-        AAUserClickLoginOutButtonCallback userClickLoginOutButtonCallback,
-        // 用户在提示界面点击确定
-        AAUserClickConfirmButtonCallback userClickConfirmButtonCallback,
-        // 注销用户
-        AALoginOutSuccessfullCallback loginOutSuccessfullCallback,
-        // 不可支付
-        AAPaymentIsRestrictedCallback paymentIsRestrictedCallback,
-        // 可以支付
-        AAPaymentUnlimitedCallback paymentUnlimitedCallback) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    internalNotification.privacyPolicyViewControllerHasBeenShownCallback = privacyPolicyViewControllerHasBeenShownCallback;
-    internalNotification.userAgreesToPrivacyPolicyCallback = userAgreesToPrivacyPolicyCallback;
-    internalNotification.loginViewControllerHasBeenShownCallback = loginViewControllerHasBeenShownCallback;
-    internalNotification.loginViewControllerHasBeenDismissedCallback = loginViewControllerHasBeenDismissedCallback;
-    internalNotification.loginSuccessCallback = loginSuccessCallback;
-    internalNotification.loginFailCallback = loginFailCallback;
-    internalNotification.userAuthVcHasBeenShownCallback = userAuthVcHasBeenShownCallback;
-    internalNotification.userAuthSuccessCallback = userAuthSuccessCallback;
-    internalNotification.warningVcHasBeenShownCallback = warningVcHasBeenShownCallback;
-    internalNotification.userClickLoginButtonInPaymentWarningVcCallback = userClickLoginButtonInPaymentWarningVcCallback;
-    internalNotification.userClickLoginButtonInNoTimeLeftWarningVcCallback = userClickLoginButtonInNoTimeLeftWarningVcCallback;
-    internalNotification.userClickLoginOutButtonCallback = userClickLoginOutButtonCallback;
-    internalNotification.userClickConfirmButtonCallback = userClickConfirmButtonCallback;
-    internalNotification.loginOutSuccessfullCallback = loginOutSuccessfullCallback;
-    internalNotification.paymentIsRestrictedCallback = paymentIsRestrictedCallback;
-    internalNotification.paymentIsRestrictedCallback = paymentIsRestrictedCallback;
+        // 实名认证失败
+        AAUserAuthFailCallback userAuthFailCallback,
+        // 游客时间已用尽
+        AANoTimeLeftWithTouristsModeCallback noTimeLeftWithTouristsModeCallback,
+        // 未成年时间已用尽
+        AANoTimeLeftWithNonageModeCallback noTimeLeftWithNonageModeCallback) {
+    AABridge *internalManager = (__bridge AABridge *)manager;
+    internalManager.loginSuccessCallback = loginSuccessCallback;
+    internalManager.loginFailCallback = loginFailCallback;
+    internalManager.userAuthSuccessCallback = userAuthSuccessCallback;
+    internalManager.userAuthFailCallback = userAuthFailCallback;
+    internalManager.noTimeLeftWithTouristsModeCallback = noTimeLeftWithTouristsModeCallback;
+    internalManager.noTimeLeftWithNonageModeCallback = noTimeLeftWithNonageModeCallback;
 }
 
-int getUserLoginStatus(AATypeNotificationRef notification) {    
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    return [internalNotification getUserLoginStatus];
+int getUserLoginStatus(AATypeRef manager) {    
+    AABridge *internalManager = (__bridge AABridge *)manager;
+    return [internalManager getUserLoginStatus];
 }
 
-int getUserAuthenticationIdentity(AATypeNotificationRef notification) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    return [internalNotification getUserAuthenticationIdentity];
+int getUserAuthenticationStatus(AATypeRef manager) {
+    AABridge *internalManager = (__bridge AABridge *)manager;
+    return [internalManager getUserAuthenticationStatus];
 }
 
-void showPrivacyPolicyView(AATypeNotificationRef notification) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification showPrivacyPolicyView];
+void presentRealNameAuthController(AATypeRef manager) {
+    AABridge *internalManager = (__bridge AABridge *)manager;
+    [internalManager presentRealNameAuthController];
 }
 
-void showLoginViewController(AATypeNotificationRef notification) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification showLoginViewController];
-}
-
-void showUserAuthenticationViewController(AATypeNotificationRef notification) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification showUserAuthenticationViewController];
-}
-
-void loginWithUserNameAndPassword(AATypeNotificationRef notification, const char *userName, const char *password) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification loginWithUserName:AAStringFromUTF8String(userName) password:AAStringFromUTF8String(password)];
-}
-
-void loginWithThirdPartyPlatform(AATypeNotificationRef notification, const char *token, const char *otherID, const char *platformName) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification loginWithPlatformToken:AAStringFromUTF8String(token) otherID:AAStringFromUTF8String(otherID) platformName:AAStringFromUTF8String(platformName)];
-}
-
-void loginWithZplayID(AATypeNotificationRef notification, const char *zplayID) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification loginWithZplayID:AAStringFromUTF8String(zplayID)];
-}
-
-void loginOut(AATypeNotificationRef notification) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification loginOut];
-}
-
-void checkNumberLimitBeforePayment(AATypeNotificationRef notification, int payNumber) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification checkNumberLimitBeforePayment:payNumber];
-}
-
-void reportNumberAfterPayment(AATypeNotificationRef notification, int payNumber) {
-    AANotificationBridge *internalNotification = (__bridge AANotificationBridge *)notification;
-    [internalNotification reportNumberAfterPayment:payNumber];
+void checkLeftTimeOfCurrentUser(AATypeRef manager) {
+    AABridge *internalManager = (__bridge AABridge *)manager;
+    [internalManager checkLeftTimeOfCurrentUser];
 }
 
 #pragma mark - Other methods
