dc83fff7c668aa14e74d1ba202f58a5845ef0dd3
JiaDingYi <wzy2010416033@163.com>
2021-05-25 16:07:15 +0800

    add aa type file

--- Assets/AASDK/Platforms/iOS/Bridge/AATypes.h
+++ Assets/AASDK/Platforms/iOS/Bridge/AATypes.h
@@ -1,38 +1,19 @@
 typedef const void *AATypeRef;
 
-#pragma mark - AANotification
-typedef const void *AATypeNotificationClientRef;
-typedef const void *AATypeNotificationRef;
-#pragma mark - AANotification callback
-// 隐私弹框已经展示
-typedef void (*AAPrivacyPolicyViewControllerHasBeenShownCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 用户同意隐私政策
-typedef void (*AAUserAgreesToPrivacyPolicyCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 开始展示用户登录界面
-typedef void (*AALoginViewControllerHasBeenShownCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 登录界面消失
-typedef void (*AALoginViewControllerHasBeenDismissedCallback)(AATypeNotificationClientRef *aaNotificationClient);
+#pragma mark - AAManager
+typedef const void *AATypeClientRef;
+typedef const void *AATypeRef;
+
+#pragma mark - callback
 // 登录成功
-typedef void (*AALoginSuccessCallback)(AATypeNotificationClientRef *aaNotificationClient, const char *zplayKey);
+typedef void (*AALoginSuccessCallback)(AATypeClientRef *aaClient, const char *touristID)
 // 登录失败
-typedef void (*AALoginFailCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 注销登录
-typedef void (*AALoginOutSuccessfullCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 实名认证界面已经展示
-typedef void (*AAUserAuthVcHasBeenShownCallback)(AATypeNotificationClientRef *aaNotificationClient);
+typedef void (*AALoginFailCallback)(AATypeClientRef *aaClient)
 // 实名认证成功
-typedef void (*AAUserAuthSuccessCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// warning vc已展示
-typedef void (*AAWarningVcHasBeenShownCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 用户支付失败时，在提示界面点击登录
-typedef void (*AAUserClickLoginButtonInPaymentWarningVcCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 用户游戏时长不足时，在提示界面点击登录
-typedef void (*AAUserClickLoginButtonInNoTimeLeftWarningVcCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 用户在提示界面点击退出游戏
-typedef void (*AAUserClickLoginOutButtonCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 用户在提示界面点击确定
-typedef void (*AAUserClickConfirmButtonCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 不可支付
-typedef void (*AAPaymentIsRestrictedCallback)(AATypeNotificationClientRef *aaNotificationClient);
-// 可以支付
-typedef void (*AAPaymentUnlimitedCallback)(AATypeNotificationClientRef *aaNotificationClient);
+typedef void (*AAUserAuthSuccessCallback)(AATypeClientRef *aaClient)
+// 实名认证失败
+typedef void (*AAUserAuthFailCallback)(AATypeClientRef *aaClient)
+// 游客时间已用尽
+typedef void (*AANoTimeLeftWithTouristsModeCallback)(AATypeClientRef *aaClient)
+// 未成年时间已用尽
+typedef void (*AANoTimeLeftWithNonageModeCallback)(AATypeClientRef *aaClient)
