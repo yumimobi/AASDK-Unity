31804da7d58f98076c61e5b30448054d5fdddfd7
JiaDingYi <wzy2010416033@163.com>
2021-05-25 17:43:10 +0800

    finetune aa types

--- Assets/AASDK/Platforms/iOS/Bridge/AATypes.h
+++ Assets/AASDK/Platforms/iOS/Bridge/AATypes.h
@@ -6,14 +6,14 @@ typedef const void *AATypeRef;
 
 #pragma mark - callback
 // 登录成功
-typedef void (*AALoginSuccessCallback)(AATypeClientRef *aaClient, const char *touristID)
+typedef void (*AALoginSuccessCallback)(AATypeClientRef *aaClient, const char *touristID);
 // 登录失败
-typedef void (*AALoginFailCallback)(AATypeClientRef *aaClient)
+typedef void (*AALoginFailCallback)(AATypeClientRef *aaClient);
 // 实名认证成功
-typedef void (*AAUserAuthSuccessCallback)(AATypeClientRef *aaClient)
+typedef void (*AAUserAuthSuccessCallback)(AATypeClientRef *aaClient);
 // 实名认证失败
-typedef void (*AAUserAuthFailCallback)(AATypeClientRef *aaClient)
+typedef void (*AAUserAuthFailCallback)(AATypeClientRef *aaClient);
 // 游客时间已用尽
-typedef void (*AANoTimeLeftWithTouristsModeCallback)(AATypeClientRef *aaClient)
+typedef void (*AANoTimeLeftWithTouristsModeCallback)(AATypeClientRef *aaClient);
 // 未成年时间已用尽
-typedef void (*AANoTimeLeftWithNonageModeCallback)(AATypeClientRef *aaClient)
+typedef void (*AANoTimeLeftWithNonageModeCallback)(AATypeClientRef *aaClient);
